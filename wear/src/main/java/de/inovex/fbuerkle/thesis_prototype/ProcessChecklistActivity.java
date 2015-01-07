b750a4c50b990cdd432bbfc21fd30fd41834252e
Felix Bürkle <f.buerkle@inovex.de>
2015-01-07 18:00:06 +0100

    created new activity to do checklist on wearable

---
+++ wear/src/main/java/de/inovex/fbuerkle/thesis_prototype/ProcessChecklistActivity.java
@@ -0,0 +1,10 @@
+package de.inovex.fbuerkle.thesis_prototype;
+
+import android.app.Activity;
+
+/**
+ * Created by felix on 07/01/15.
+ */
+public class ProcessChecklistActivity extends Activity {
+	
+}
