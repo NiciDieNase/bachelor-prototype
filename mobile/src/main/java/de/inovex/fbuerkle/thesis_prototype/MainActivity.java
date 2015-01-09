9e8d52511be8edcc9a196fb49f2036ba12700f37
Felix Bürkle <f.buerkle@inovex.de>
2015-01-09 18:54:08 +0100

    removed sync-stuff from handheld-main-activity

--- mobile/src/main/java/de/inovex/fbuerkle/thesis_prototype/MainActivity.java
+++ mobile/src/main/java/de/inovex/fbuerkle/thesis_prototype/MainActivity.java
@@ -3,39 +3,20 @@ package de.inovex.fbuerkle.thesis_prototype;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.activeandroid.query.Select;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.PutDataMapRequest;
-import com.google.android.gms.wearable.PutDataRequest;
-import com.google.android.gms.wearable.Wearable;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import de.inovex.fbuerkle.DataKeys;
 import de.inovex.fbuerkle.datamodel.Checklist;
 
 
 public class MainActivity extends Activity{
 
 	private static final String TAG = "de.inovex.fbuerkle.checklist";
-
-	public GoogleApiClient getmGoogleApiClient() {
-		return mGoogleApiClient;
-	}
-
-	private GoogleApiClient mGoogleApiClient;
 
 	public Checklist currentChecklist;
 
@@ -63,29 +44,6 @@ public class MainActivity extends Activity{
 		ChecklistAdapter adapter = new ChecklistAdapter(this, currentChecklist);
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(adapter);
-
-		mGoogleApiClient = new GoogleApiClient.Builder(this)
-        .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                @Override
-                public void onConnected(Bundle connectionHint) {
-                    Log.d(TAG, "onConnected: " + connectionHint);
-                    // Now you can use the Data Layer API
-                }
-                @Override
-                public void onConnectionSuspended(int cause) {
-                    Log.d(TAG, "onConnectionSuspended: " + cause);
-                }
-        })
-        .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
-                @Override
-                public void onConnectionFailed(ConnectionResult result) {
-                    Log.d(TAG, "onConnectionFailed: " + result);
-                }
-            })
-        // Request access only to the Wearable API
-        .addApi(Wearable.API)
-        .build();
-		mGoogleApiClient.connect();
 	}
 
 
@@ -124,35 +82,7 @@ public class MainActivity extends Activity{
 				builder.show();
 				return true;
 			case R.id.action_sync:
-				new AsyncTask(){
-					@Override
-					protected Object doInBackground(Object[] params) {
-						List<Checklist> checklists =  new Select().from(Checklist.class).execute();
-						ArrayList<String> listNames = new ArrayList<String>();
-
-						for(Checklist list : checklists){
-							PutDataMapRequest dataMap = PutDataMapRequest.create("/checklists/"+list.name.toLowerCase());
-							dataMap.getDataMap().putString(DataKeys.name,list.name);
-							dataMap.getDataMap().putString(DataKeys.description,list.description);
-							dataMap.getDataMap().putString(DataKeys.type,"Checklist");
-							PutDataRequest request = dataMap.asPutDataRequest();
-							PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mGoogleApiClient,request);
-							listNames.add(list.name);
-						}
-
-						PutDataMapRequest dataMap = PutDataMapRequest.create("/checklists");
-						dataMap.getDataMap().putStringArrayList("checklists",listNames);
-						PutDataRequest request = dataMap.asPutDataRequest();
-						PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mGoogleApiClient,request);
-						return null;
-					}
-
-					@Override
-					protected void onPostExecute(Object o) {
-						super.onPostExecute(o);
-						Toast.makeText(MainActivity.this,"Current Checklist Synced",Toast.LENGTH_SHORT).show();
-					}
-				}.execute();
+				// TODO sync checklists
 				return true;
 			case R.id.action_add_checklist:
 				new NewChecklistFragment().show(getFragmentManager(),"newChecklistFragment");
